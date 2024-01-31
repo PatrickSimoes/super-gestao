9c99a8cbcce82db3f3869cf595bc154ee8825015
Patrick Simões <patrick@tss.inf.br>
2024-01-30 23:34:45 -0300

    Sessão-12: Criando componente blade

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -19,28 +19,8 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form method="post" action="{{ route('produto.store') }}">
-                    @csrf
-                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-                    <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder="Descrição" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                    <input type="text" name="peso" value="{{ old('peso') }}" placeholder="peso" class="borda-preta">
-                    {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-
-                    <select name="unidade_id">
-                        <option>-- Selecione a Unidade de Medida --</option>
-
-                        @foreach ($unidades as $unidade)
-                            <option value="{{ $unidade->id }} {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}">{{ $unidade->descricao }}</option>
-                        @endforeach
-                    </select>
-                    {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @component('app.produto._components.forn_create_edit', ['unidades' => $unidades])
+                @endcomponent
             </div>
         </div>
 
